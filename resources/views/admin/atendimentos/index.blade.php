585bfe789d01ddf25537f38b94e0f555c2590406
Cedemir Pereira <cedemir@gmail.com>
2022-01-06 09:56:17 -0300

    Update

--- resources/views/admin/atendimentos/index.blade.php
+++ resources/views/admin/atendimentos/index.blade.php
@@ -34,14 +34,14 @@ Meus Atendimentos
                 <?php 
                 $forma_atendimento=App\Models\Forma_atendimento::where('id','=',$atendimento->atendimento_id)->first();
                 $aluno=App\Models\Aluno::where('id','=',$atendimento->aluno_id)->first();
-                $forma_atendimento=App\Models\User::where('id','=',$atendimento->user_id)->first();
+                $user=App\Models\User::where('id','=',$atendimento->user_id)->first();
                  ?> 
                 <tr>
                     <td>{{$atendimento->id}} </td>
                     <td>{{$atendimento->data->format('d/m/Y')}}</td>
                     <td>{{$atendimento->hora}}</td>
-                    <td>{{$atendimento->aluno_id}}</td>
-                    <td>{{$atendimento->user_id}}</td>
+                    <td>{{$aluno->nome}}</td>
+                    <td>{{$user->name}}</td>
                     <td>{{$forma_atendimento->forma_atendimento}}</td>
                     
                     <td><a href="{{route('admin.atendimentos.edit',$atendimento->id)}}" class="btn btn-warning">Editar</a>
