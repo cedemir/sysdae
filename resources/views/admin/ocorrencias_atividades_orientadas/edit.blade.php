0aa47f191f98ad1a7e2920804f4fbe7d6cb70e40
Cedemir Pereira <cedemir@gmail.com>
2022-01-06 17:53:54 -0300

    ocorrencias fix

--- resources/views/admin/ocorrencias_atividades_orientadas/edit.blade.php
+++ resources/views/admin/ocorrencias_atividades_orientadas/edit.blade.php
@@ -31,6 +31,19 @@ Atualizar Atividade Orientada
         </div>
 
         <div class="form-group">
+            <label > Aluno </label>
+            <select class="form-control" name="aluno_id" id="aluno_id"  required>
+                <option value=""> Selecione o Aluno</option>
+                @foreach($alunos as $aluno)
+                <option value="{{ $aluno->id }}" {{$ocorrencias_atividades_orientadas->aluno_id == $aluno->id  ? 
+                    'selected' : ''}}>{{ $aluno->nome}}</option>
+    
+                @endforeach
+            </select>
+        </div>
+
+
+        <div class="form-group">
             <label > Setor </label>
             <select class="form-control" name="setor_id" id="setor_id"  required>
                 <option value=""> Selecione o Setor</option>
@@ -43,6 +56,18 @@ Atualizar Atividade Orientada
         </div>
     
         <div class="form-group">
+            <label > Servidor </label>
+            <select class="form-control" name="user_id" id="user_id"  required>
+                <option value=""> Selecione o Servidor</option>
+                @foreach($user as $user)
+                <option value="{{ $user->id }}" {{$ocorrencias_atividades_orientadas->user_id == $user->id  ? 
+                    'selected' : ''}}>{{ $user->name}}</option>
+    
+                @endforeach
+            </select>
+        </div>
+
+        <div class="form-group">
             <label>Data </label>
             <input type="text" class="form-control @error('data') is-invalid @enderror" name="data" value="{{$ocorrencias_atividades_orientadas->data}}">
             @error('data')
@@ -52,18 +77,20 @@ Atualizar Atividade Orientada
             @enderror
         </div>
 
+
         <div class="form-group">
-            <label>Servidor </label>
-            <input type="text" class="form-control @error('servidor') is-invalid @enderror" name="servidor" value="{{$ocorrencias_atividades_orientadas->servidor}}">
-            @error('servidor')
+            <label>Descrição das Atividades</label>
+            <textarea class="form-control @error('descricao_atividade') is-invalid @enderror" name="descricao_atividade" id="descricao_atividade" cols="80" rows="10">{{$ocorrencias_atividades_orientadas->descricao_atividade}}</textarea>
+            @error('descricao_atividade')
             <div class="alert alert-danger">
                 {{$message}}
             </div>
             @enderror
         </div>
 
+
         <div class="form-group">
-            <label>Horas </label>
+            <label>Horas Cumpridas</label>
             <input type="text" class="form-control @error('nro_horas') is-invalid @enderror" name="nro_horas" value="{{$ocorrencias_atividades_orientadas->nro_horas}}">
             @error('nro_horas')
             <div class="alert alert-danger">
